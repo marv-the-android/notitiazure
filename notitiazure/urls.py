3c90d293a47c47b44c2cd04972bb28d50f08fcf6
marv-the-android <55961009+marv-the-android@users.noreply.github.com>
2019-10-01 13:38:35 +0100

    Update urls.py

--- notitiazure/urls.py
+++ notitiazure/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainmap.views import mainMap
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
