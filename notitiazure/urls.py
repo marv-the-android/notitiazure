071b49e10f69292b60afb90555f514c16193e9e4
marv-the-android <55961009+marv-the-android@users.noreply.github.com>
2019-10-01 20:31:15 +0100

    Update urls.py

--- notitiazure/urls.py
+++ notitiazure/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainmap.views import mainMap
-from django.views.static import serve
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('map/', mainMap),
-    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
